2d6c06140e04f2c76b4074e0991f665b13ce8548
simonebuglione <si.buglione96@gmail.com>
2024-11-05 17:51:03 +0100

    Create ESERCIZIO TEATRO.py
    Traccia esercitazione

---
+++ Corso Python e ML/05-11/ESERCIZIO TEATRO.py
@@ -0,0 +1,23 @@
+"""creare una classe base Posto che rappresenta un singolo posto nel teatro. Da questa, deriveranno diverse classi per tipi specifici di posti, come postoVIP e PostoStandard. Sarà inoltre necessaria una classe Teatro per gestire tutti i posti e le prenotazioni.
+
+1.Classe Posto:
+-_numero (intero: numero del posto)
+-_fila (stringa: fila in cui si trova il posto)
+-_occupato (booleano: stato del posto, se è occupato o meno)
+Metodi:
+-prenota(): prenota il posto se non è già occupato.
+libera(): libera il posto se è occupato.
+Getter per numero e fila, e uno stato che indica se il posto è occupato.
+2. Classi Derivate: 
+PostoVIP:
+-Aggiunge un attributo per servizi_extra (e.g., accesso al lounge, servizio in posto).
+-Sovrascrive il metodo prenota() per includere la gestione dei servizi extra.
+PostoStandard:
+-Potrebbe avere un costo aggiuntivo per la prenotazione online o altri servizi meno esclusivi.
+3. Classe Teatro:
+Attributi:
+-_posti: lista di tutti i posti nel teatro.
+Metodi:
+-prenota_posto(numero, fila): trova e prenota un posto specifico.
+-stampa_posti_occupati(): mostra tutti i posti occupati"""
+
