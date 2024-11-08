c342da0fc00e21a524aa0857f4e55960407bab21
simonebuglione <si.buglione96@gmail.com>
2024-11-08 12:09:41 +0100

    -

--- Corso Python e ML/08-11/ESERCIZIO ARRAY.py
+++ Corso Python e ML/08-11/ESERCIZIO ARRAY.py
@@ -20,4 +20,39 @@ array_float=array.astype(np.float64) #converto numeri interi in decimali
 print("tipo di dato array dopo il cambiamenot:", array_float.dtype)
 
 #stampa forma array
-print("forma array:", array.shape)
+print("forma array:", array.shape);
+
+
+#CON OGGETTI:
+#definisco una classe che gestisce array
+class ArrayManipulator:
+    def __init__(self,start, stop):
+        self.array=np.arrange(start,stop)
+        
+    #metodo per ottenere tipo di dato array
+    def get_dtype(self):
+        return self.array.dtype
+    
+    #metodo per cambiare tipo di dato array
+    def change_dtype(self, new_dtype):
+        self.array= self.array.astype(new_dtype)
+
+    #metodo per ottendere forma array
+    def get_shape(self):
+        return self.array.shape
+    
+
+    
+    
+    #creo istanza classse arraymanipulator
+    manipulator = ArrayManipulator(10, 50)
+
+    #verifica tipo dato array
+    print("tipo dato array:", manipulator.get_dtype()) #tipo iniziale
+
+    #cambia tipo dato array a float64
+    manipulator.change_dtype(np.float64)
+    print("tipo dato array dopo il cambiamento:", manipulator.get_dtype()) #tipo dopo il cambiamento
+
+    #stampo forma array
+    print("forma array:", manipulator.get_shape())#forma array
