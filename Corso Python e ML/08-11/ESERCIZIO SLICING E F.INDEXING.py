edabc3bfb9136f46199ce2e7c60fc7e6d65f928a
simonebuglione <si.buglione96@gmail.com>
2024-11-08 12:19:47 +0100

    rendo attributi privati

--- Corso Python e ML/08-11/ESERCIZIO SLICING E F.INDEXING.py
+++ Corso Python e ML/08-11/ESERCIZIO SLICING E F.INDEXING.py
@@ -21,35 +21,37 @@ class MatriceManipolata:
     def __init__(self, seed=None):
                  #inizializza la classe e crea una matrice 6x6 con numeri random tra 1 e 100
                  np.random.seed(seed) #imposta replicabilità ma in modo deterministico
-                 self.matrice_originale=np.random.randint(1,101, size=(6,6))
+                 self.__matrice_originale=np.random.randint(1,101, size=(6,6))
 
     def estrai_sottomatrice(self):
             #estrae sottomatrice centrale 4x4 da matrice originale
-            self.sotto_matrice_centrale= self.matrice_originale[1:5, 1:5] #1 5 perchè toglie 1, quindi è 1 a 4
-            return self.sotto_matrice_centrale
+            self.__sotto_matrice_centrale= self.__matrice_originale[1:5, 1:5] #1 5 perchè toglie 1, quindi è 1 a 4
+            return self.__sotto_matrice_centrale
     
     def inverti_righe(self):
             #inverte righe della sotto matrice centrale
-            self.sotto_matrice_invertita=self.sotto_matrice_centrale[::-1]
-            return self.sotto_matrice_invertita
+            self.__sotto_matrice_invertita=self.__sotto_matrice_centrale[::-1]
+            return self.__sotto_matrice_invertita
     
     def estrai_diagonale(self):
             #estrae diagonale principale dalla matrice invertita
-            self.diagonale_principale=np.diagonal(self.sotto_matrice_centrale)
-            return self.diagonale_principale
+            self.__diagonale_principale=np.diagonal(self.__sotto_matrice_centrale)
+            return self.__diagonale_principale
+    
+
     def modifica_matrice(self):
             #sostituisce elementi multipli di 3 con -1 nella matr invertita
-            self.matrice_invertita_modificata=self.sotto_matrice_invertita.copy()
-            self.matrice_invertita_modificata[self.matrice_invertita_modificata % 3 == 0] = -1 #sostituisco elementi matrice invertita e la modifico
-            return self.matrice_invertita_modificata
+            self.__matrice_invertita_modificata=self.__sotto_matrice_invertita.copy()
+            self.__matrice_invertita_modificata[self.__matrice_invertita_modificata % 3 == 0] = -1 #sostituisco elementi matrice invertita e la modifico
+            return self.__matrice_invertita_modificata
     
     def stampa_risultati(self):
             #stampa risultati
-            print("matrice originale:", self.matrice_originale)
-            print("sotto-matrice centrale 4x4:", self.sotto_matrice_centrale)
-            print("matrice invertita:", self.sotto_matrice_invertita)            
-            print("diagonale principale:", self.diagonale_principale)
-            print("matrice invertita modificata:", self.matrice_invertita_modificata)
+            print("matrice originale:", self.__matrice_originale)
+            print("sotto-matrice centrale 4x4:", self.__sotto_matrice_centrale)
+            print("matrice invertita:", self.__sotto_matrice_invertita)            
+            print("diagonale principale:", self.__diagonale_principale)
+            print("matrice invertita modificata:", self.__matrice_invertita_modificata)
 
 #esecuzione programma per replicabilità
 def eseguire_programma():
