0142c13ce1e2e275f447c508cb6440653560f2af
simonebuglione <si.buglione96@gmail.com>
2024-11-13 14:56:49 +0100

    Update IMPORTA MYSQL IN PY.py

--- Corso Python e ML/12-11 (SQL)/IMPORTA MYSQL IN PY.py
+++ Corso Python e ML/12-11 (SQL)/IMPORTA MYSQL IN PY.py
@@ -8,7 +8,29 @@ mydb=mysql.connector.connect(
 )
 
 mycursor=mydb.cursor()
+
+
+#creo database
 query="create database pythonmysql"
-mycursor.execute
+mycursor.execute(query)
+
+
+#visualizzo dataabse esistenti
+query="show databases"
+mycursor.execute(query) #esegue query per mostrare i database
+
+#stampa dei risultaati
+for x in mycursor:
+    print(x)
 
 print(mydb)
+
+#creo una query per creare una tabella
+#query="create table if not exist utenti (nome varchar(50), indirizzo varchar(50))"
+
+query ="insert into utenti (nome, indirizzo) values(%s%s)"
+valori=("simone", "via roma")
+
+mycursor=mydb.cursor()
+
+mycursor.execute(query)
