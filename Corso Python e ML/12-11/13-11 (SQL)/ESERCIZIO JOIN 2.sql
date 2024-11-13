5dcb7eb9acb7ee6bf01c8b59afb834b19c90abf4
simonebuglione <si.buglione96@gmail.com>
2024-11-13 09:52:32 +0100

    Create ESERCIZIO JOIN 2.sql

---
+++ Corso Python e ML/12-11/13-11 (SQL)/ESERCIZIO JOIN 2.sql
@@ -0,0 +1,13 @@
+-- Si vuole recuperare il numero di città per nazione dal database "world" mostrando anche il nome della nazione e ordinando il risultato in base al numero di città.
+
+SELECT 
+    Country.Name AS Nazione,
+    COUNT(City.ID) AS NumeroCittà
+FROM 
+    Country
+JOIN 
+    City ON Country.Code = City.CountryCode
+GROUP BY 
+    Country.Name
+ORDER BY 
+    NumeroCittà DESC;
