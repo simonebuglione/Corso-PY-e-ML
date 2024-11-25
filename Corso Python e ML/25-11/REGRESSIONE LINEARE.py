626b20ca32d16413b2002bda5e9b197da77a7f9d
simonebuglione <si.buglione96@gmail.com>
2024-11-25 16:52:55 +0100

    Update REGRESSIONE LINEARE.py

--- Corso Python e ML/25-11/REGRESSIONE LINEARE.py
+++ Corso Python e ML/25-11/REGRESSIONE LINEARE.py
@@ -3,4 +3,12 @@
 from sklearn.linear_model import LinearRegression
 model = LinearRegression()
 model.fit(X, y)
-PREDICTIONS = model.predict(X)
+predictions = model.predict(X)
+
+
+#k-neighbors (KNN)
+
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors=3)
+knn.fit(X, y)
+predictione = knn.predict(X)
