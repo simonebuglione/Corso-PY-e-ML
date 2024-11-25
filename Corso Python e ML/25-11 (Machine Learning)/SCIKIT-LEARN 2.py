14af0cf4772534ef2e5e48dc4d75a369c13b9874
simonebuglione <si.buglione96@gmail.com>
2024-11-25 16:54:35 +0100

    -

--- Corso Python e ML/25-11 (Machine Learning)/SCIKIT-LEARN 2.py
+++ Corso Python e ML/25-11 (Machine Learning)/SCIKIT-LEARN 2.py
@@ -11,3 +11,19 @@ y = data.target  # target
 
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
+
+
+#regressione lineare
+
+from sklearn.linear_model import LinearRegression
+model = LinearRegression()
+model.fit(X, y)
+predictions = model.predict(X)
+
+
+#k-neighbors (KNN)
+
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors=3)
+knn.fit(X, y)
+predictione = knn.predict(X)
